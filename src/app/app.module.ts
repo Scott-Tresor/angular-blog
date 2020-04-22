5845e7a24ecd651d85888cb5944f8532d1bdf6e4
scott-Tresor <scotttresor@gmail.com>
2020-04-22 16:59:25 +0200

    feat:review a latest blog system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BlogModule } from './blog/BlogModul';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    MatButtonModule,
-    MatIconModule,
-    BlogModule,
 
   ],
   providers: [],
