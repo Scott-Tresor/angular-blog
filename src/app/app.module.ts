021ff9a6928fac4c00e13afb63af4edc92fd36f3
scott-Tresor <scotttresor@gmail.com>
2020-04-23 00:04:54 +0200

    feat:add a navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Route } from './app.routing';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+import { HeaderComponent } from './Component/header/header.component';
+import { ErrorComponent } from './Component/error/error.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContactComponent } from './pages/contact/contact.component';
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    ErrorComponent,
+    DashboardComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
-import {NgModule} from '@angular/core';
+import { ErrorComponent } from './Component/error/error.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 
 const ROUTING_PATH: Routes = [
-    { path: '', component: '' },
+    { path:'', component: DashboardComponent},
+    { path:'contact', component: ContactComponent},
+    { path: '**', component: ErrorComponent }
 ];
 
 export const Route = RouterModule.forRoot(ROUTING_PATH);
