8d8b036d242d048261256913c8b6d7737e035c21
scott-Tresor <scotttresor@gmail.com>
2020-05-07 17:45:22 +0200

    ajout de la page not found

--- src/app/models/api.model.ts
+++ src/app/models/api.model.ts
@@ -1,18 +1,19 @@
-
 export interface Contact {
     username:string;
     email:string;
     phones:number;
     content:string;
 }
-
 export interface Impots {
     username:string;
     lastname:string;
     prenom: string;
     phones:number;
 }
-
 export interface newsletter {
     email: string;
+}
+
+export interface Maps {
+
 }
