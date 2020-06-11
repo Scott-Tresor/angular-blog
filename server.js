c3052aaa611b981406d3f31372d8ac9f1af0167e
scott-Tresor <scotttresor@gmail.com>
2020-06-11 19:05:46 +0200

    end

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/drhkat'));
+app.use(express.static(__dirname + '/dist/blog'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/drhkat/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/blog/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
